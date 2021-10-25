50cd33c295f1dc925e177a429926a4f30881e9be
keechy1231 <76797169+keechy1231@users.noreply.github.com>
2021-10-25 22:22:44 +0100

    Add files via upload

---
+++ enemy.py
@@ -0,0 +1,42 @@
+import random
+import math
+import time
+
+#this will be the base stats of the Enemies
+Ehealth = 5
+Estamina = 5
+Emana = 0
+Estealth = 0
+Estrength = 0
+
+class Imp
+	Ehealth * 1
+	Estamina * 1
+	Emana * 1 
+	Estealth * 1
+	Estrength * 1
+	
+	
+class Dwarf
+	Ehealth * 2
+	Estamina * 1
+	Emana * 1 
+	Estealth * 1
+	Estrength * 3
+
+
+class Ogre
+	Ehealth * 3
+	Estamina * 0.3
+	Emana * 1
+	Estealth * 0
+	Estrength * 5
+	
+	
+
+
+			
+			
+		
+		
+		
