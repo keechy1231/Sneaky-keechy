0616117c2498218142bcd7735a4933d2a0ca37b6
keechy1231 <76797169+keechy1231@users.noreply.github.com>
2021-10-25 22:38:08 +0100

    Add files via upload

--- enemy.py
+++ enemy.py
@@ -3,35 +3,38 @@ import math
 import time
 
 #this will be the base stats of the Enemies
-Ehealth = 5
-Estamina = 5
-Emana = 0
-Estealth = 0
-Estrength = 0
+Ehealth = 10
+Estamina = 10
+Emana = 10
+Estealth = 10
+Estrength = 10
 
-class Imp
-	Ehealth * 1
-	Estamina * 1
-	Emana * 1 
-	Estealth * 1
-	Estrength * 1
+class Imp:
+	Ehealth = Ehealth * 0.5
+	Estamina = Ehealth * 0.2
+	Emana = Emana * 0.2 
+	Estealth = Estealth * 0.2
+	Estrength = Estrength * 0.2
 	
 	
-class Dwarf
-	Ehealth * 2
-	Estamina * 1
-	Emana * 1 
-	Estealth * 1
-	Estrength * 3
+class Dwarf:
+	Ehealth = Ehealth * 2
+	Estamina = Ehealth * 2
+	Emana = Emana * 2
+	Estealth = Estealth * 2
+	Estrength = Estrength * 2
 
 
-class Ogre
-	Ehealth * 3
-	Estamina * 0.3
-	Emana * 1
-	Estealth * 0
-	Estrength * 5
+class Ogre:
+	EEhealth = Ehealth * 3
+	Estamina = Ehealth * 3
+	Emana = Emana * 0
+	Estealth = Estealth * 3
+	Estrength = Estrength * 3
 	
+	
+print (Imp.Estamina
+)
 	
 
 
