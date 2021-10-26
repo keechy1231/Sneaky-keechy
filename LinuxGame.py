743dbdbce8d76909d4c1658d3914a6085f8fc811
keechy1231 <76797169+keechy1231@users.noreply.github.com>
2021-10-26 19:46:25 +0100

    Add files via upload

---
+++ LinuxGame.py
@@ -0,0 +1,45 @@
+#this is the main game
+
+import os
+import time #importing time
+import sys
+
+
+def typing(text):
+	for character in text:
+		sys.stdout.write(character)
+		sys.stdout.flush()
+		time.sleep(0.05)
+		
+time.sleep(7)		
+
+typing ("Welcome to our first little game\n\n\n\n\nCreated by;")
+
+time.sleep(1)
+
+mystring = '''			:::    ::: :::::::::: :::::::::: ::::::::  :::    ::: :::   :::           :::     ::::    ::: :::::::::         ::::::::  ::::    ::: ::::::::::     :::     :::    ::: :::   ::: 
+				:+:   :+:  :+:        :+:       :+:    :+: :+:    :+: :+:   :+:         :+: :+:   :+:+:   :+: :+:    :+:       :+:    :+: :+:+:   :+: :+:          :+: :+:   :+:   :+:  :+:   :+: 
+				+:+  +:+   +:+        +:+       +:+        +:+    +:+  +:+ +:+         +:+   +:+  :+:+:+  +:+ +:+    +:+       +:+        :+:+:+  +:+ +:+         +:+   +:+  +:+  +:+    +:+ +:+  
+				+#++:++    +#++:++#   +#++:++#  +#+        +#++:++#++   +#++:         +#++:++#++: +#+ +:+ +#+ +#+    +:+       +#++:++#++ +#+ +:+ +#+ +#++:++#   +#++:++#++: +#++:++      +#++:   
+				+#+  +#+   +#+        +#+       +#+        +#+    +#+    +#+          +#+     +#+ +#+  +#+#+# +#+    +#+              +#+ +#+  +#+#+# +#+        +#+     +#+ +#+  +#+      +#+    
+				#+#   #+#  #+#        #+#       #+#    #+# #+#    #+#    #+#          #+#     #+# #+#   #+#+# #+#    #+#       #+#    #+# #+#   #+#+# #+#        #+#     #+# #+#   #+#     #+#    
+				###    ### ########## ########## ########  ###    ###    ###          ###     ### ###    #### #########         ########  ###    #### ########## ###     ### ###    ###    ###    '''			
+
+print (mystring)	
+
+
+time.sleep(5)
+os.system("clear")
+
+typing ("In this game you will encounter random events against random enemies, everytime you play this game it will play differently.\n\n\n\n\n")
+
+
+input ("Press Enter to Begin")
+
+os.system("clear")
+from Prologue import Prologue #imorting Prologue into this #bring Prologue to the game
+Prologue()
+
+
+
+
