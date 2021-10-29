6a7124228020754aa940db7facfcc33b90f869cf
tomas keech <keechy1231@hotmail.co.uk>
2021-10-29 23:31:57 +0100

    changes mde to alltogetherworking

--- inventory.py
+++ inventory.py
@@ -8,7 +8,7 @@ char = {'lvl': 1,
          'HP' : 10}
 
 
-inv = {'ATKpotion': 10,
+inventory = {'ATKpotion': 10,
         'HPpotion': 10}
 
 HPpotion = inv['HPpotion']
@@ -18,15 +18,9 @@ ATKpotion = inv['ATKpotion']
 def useHPpotion(HPpotion):
 
     print ("you are using HP potion")
-    nhp = inv['HPpotion'] + char['HP']
+    nhp = inventory['HPpotion'] + char['HP']
     print (nhp)
 
-
-
-def useATKpotion(ATKpotion):
-    print ("you are using an Attack potion")
-    nATK = inv['ATKpotion']+char['ATK'] 
-    print (nATK)   
 
 useHPpotion(HPpotion)
 
